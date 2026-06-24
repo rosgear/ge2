db71fb9323d491a06a82bf4b39170dc333149eea
Anton Tivonenko <rosgear.it@gmail.com>
2026-06-24 10:24:08 +0300

    Locale

---
+++ framework/Theme/Info/patterns/en_GB.php
@@ -0,0 +1,57 @@
+<?php
+/**
+ * Этот файл является частью пакета Ge Framework.
+ * 
+ * Пакет английской (британской) локализации описания шаблонов..
+ * 
+ * @link https://rosgear.ru/framework/
+ * @copyright Copyright (c) 2015 RosGear
+ * @license https://rosgear.ru/license/
+ */
+
+return [
+    'Default' => 'Default',
+    'Database' => 'Database',
+    'Settings' => 'Settings',
+    'Form' => 'Form',
+    'Grid' => 'Grid',
+    'Tree' => 'Tree',
+    'Selection form' => 'Selection form',
+    'Account' => 'Account',
+    'Help' => 'Help',
+    'Common' => 'Common',
+    'Articles' => 'Articles',
+    'Article' => 'Article',
+    'Partials' => 'Partials',
+    'Sections' => 'Sections',
+    'Section' => 'Section',
+    'Pages' => 'Pages',
+    'Page' => 'Страница',
+    'Layouts' => 'Layouts',
+    'Layout' => 'Layout',
+    'Errors' => 'Page errors',
+    'Error' => 'Error',
+    'Widgets' => 'Widgets',
+    'Widget' => 'Widget',
+    'Plugins' => 'Plugins',
+    'Plugin' => 'Плагины',
+    'Mails' => 'Mails',
+    'Mail' => 'Mail',
+    'Components' => 'Components',
+    'Component' => 'Component',
+    'Files' => 'Files',
+    'File' => 'File',
+    'Folders' => 'Folders',
+    'Folder' => 'Folder',
+    'Modules' => 'Modules', 
+    'Module' => 'Module', 
+    'Views' => 'Views',
+    'View' => 'View',
+    'Permission' => 'Permission',
+    'Text' => 'Text',
+    'Description' => 'Description',
+    'Extension' => 'Module extensions',
+    'Languages' => 'Languages',
+    'Language' => 'Language',
+    'Ge' => 'RosGear'
+];
---
+++ framework/Theme/Info/patterns/ru_RU.php
@@ -0,0 +1,57 @@
+<?php
+/**
+ * Этот файл является частью пакета Ge Framework.
+ * 
+ * Пакет русской локализации описания шаблонов.
+ * 
+ * @link https://rosgear.ru/framework/
+ * @copyright Copyright (c) 2015 RosGear
+ * @license https://rosgear.ru/license/
+ */
+
+return [
+    'Default' => 'По умолчанию',
+    'Database' => 'База данных',
+    'Settings' => 'Настройки',
+    'Form' => 'Форма',
+    'Grid' => 'Список',
+    'Tree' => 'Дерево',
+    'Selection form' => 'Форма выбора',
+    'Account' => 'Аккаунт',
+    'Help' => 'Помощь',
+    'Common' => 'Общий',
+    'Articles' => 'Статьи',
+    'Article' => 'Статья',
+    'Partials' => 'Части шаблонов',
+    'Sections' => 'Разделы',
+    'Section' => 'Раздел',
+    'Pages' => 'Страницы сайта',
+    'Page' => 'Страница',
+    'Layouts' => 'Макеты страниц сайта',
+    'Layout' => 'Макет',
+    'Errors' => 'Страницы ошибок',
+    'Error' => 'Ошибка',
+    'Widgets' => 'Виджеты',
+    'Widget' => 'Виджет',
+    'Plugins' => 'Плагины',
+    'Plugin' => 'Плагины',
+    'Mails' => 'Письма',
+    'Mail' => 'Письмо',
+    'Components' => 'Компоненты',
+    'Component' => 'Компонент',
+    'Files' => 'Файлы',
+    'File' => 'Файл',
+    'Folders' => 'Каталоги',
+    'Folder' => 'Каталог',
+    'Modules' => 'Модули', 
+    'Module' => 'Модуль', 
+    'Views' => 'Шаблоны',
+    'View' => 'Шаблон',
+    'Permission' => 'Права доступа',
+    'Text' => 'Текст',
+    'Description' => 'Описание',
+    'Extension' => 'Расширение модуля',
+    'Languages' => 'Языки',
+    'Language' => 'Язык',
+    'Ge' => 'RosGear'
+];

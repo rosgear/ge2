29690910ad5126c0b0f77089eaf002a755fdeb4e
Anton Tivonenko <rosgear.it@gmail.com>
2026-06-24 11:04:53 +0300

    Exception

---
+++ framework/Data/Exception/InvalidArgumentException.php
@@ -0,0 +1,23 @@
+<?php
+/**
+ * Этот файл является частью пакета Ge Framework.
+ * 
+ * @link https://rosgear.ru/framework/
+ * @copyright Copyright (c) 2015 RosGear
+ * @license https://rosgear.ru/license/
+ */
+
+namespace Ge\Data\Exception;
+
+use Ge\Exception;
+
+/**
+ * {@inheritdoc}
+ * 
+ * @author Anton Tivonenko <anton.tivonenko@gmail.com>
+ * @package Ge\Data\Exception
+ * @since 2.0
+ */
+class InvalidArgumentException extends Exception\InvalidArgumentException
+{
+}
